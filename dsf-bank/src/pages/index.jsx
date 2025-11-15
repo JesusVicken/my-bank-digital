a117f44f62d6f8f0c1f7f3912657f9a896401eb0
JesusVicken <jesus.viccken@gmail.com>
2025-11-15 15:12:42 -0300

    feat: add nova logo/2

--- dsf-bank/src/pages/index.jsx
+++ dsf-bank/src/pages/index.jsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>JESUS Bank</title>
+        <title>JESUS Banks</title>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
       <Navbar />
